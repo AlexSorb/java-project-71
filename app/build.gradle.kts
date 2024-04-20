da1ff225906e62d7c4f7b960b0a3b00ef648e0f0
Alexandr Ryabinin <zochsa@gmail.com>
2024-04-20 21:57:07 +0300

    Integrated picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.5")
+   // annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
+description = "Compares two configuration files and shows a difference.")
 public class App {
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
         System.out.println("Hello World!");
     }
 }
