ce59a994fae2531fbac7643df8c1f6e753cfdf19
Alexandr Ryabinin <zochsa@gmail.com>
2024-04-27 15:49:32 +0300

    Add make

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,12 @@ repositories {
 }
 
 dependencies {
+    // Работа с picocli
     implementation("info.picocli:picocli:4.7.5")
-   // annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
+
+    // Работа с Jackson ObjectMapper
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,18 @@
 package hexlet.code;
 
+import hexlet.code.source.FileManager;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
-import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+import hexlet.code.source.Differ;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
 description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable<Integer> {
 
     @Option(names = {"-f", "--format"}, paramLabel = "format",
             description = "output format [default: stylish]" //,
@@ -18,15 +21,27 @@ public class App {
     private String format = "stylish";
 
 
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
-    private Path filepath1;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1 = "/home/alex/java-project-71/app/File1.json";
 
-    @Parameters(paramLabel = "filepath2", description = "path to second file")
-    private Path filepath2;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2 = "/home/alex/java-project-71/app/File2.json";
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        //new CommandLine(this).parseArgs(args);
-        System.out.println("Hello World!");
+        App app = new App();
+        int exitCode = new CommandLine(app).execute(args);
+       // new CommandLine(app).parseArgs(args);
+
+        app.call();
+
+        System.exit(exitCode);
+    }
+
+    public Integer call() {
+            var readfile1 = Differ.readJson(FileManager.normaolizePath(this.filepath1));
+            var readfile2 = Differ.readJson(FileManager.normaolizePath(this.filepath2));
+            var comparison = Differ.generate(readfile1, readfile2);
+            System.out.println(comparison);
+        return null;
     }
 }
