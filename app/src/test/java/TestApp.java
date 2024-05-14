811ff6655820444e151616fa1ae25d1a17dffedd
Alexandr Ryabinin <zochsa@gmail.com>
2024-05-14 13:17:32 +0300

    Create tests

--- app/src/test/java/TestApp.java
+++ app/src/test/java/TestApp.java
@@ -3,15 +3,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class TestApp {
-    public static Map<String, String> firstJson;
-    public static Map<String, String> secondJson;
+    public static Map<String, Object> firstJson;
+    public static Map<String, Object> secondJson;
 
     @BeforeAll
-    public static void generate(){
+    public static void generate() {
         firstJson = Map.of(
                 "host", "hexlet.io",
                 "timeout", "50",
@@ -26,17 +25,18 @@ public class TestApp {
         );
     }
     @Test
-    public void testApp() {
+    public void testParsing() {
         System.out.println("Test1");
-        StringBuilder rightAnser = new StringBuilder("{\n")
-                .append( "- follow: false\n")
-                .append("host: hexlet.io\n")
-                .append("- proxy: 123.234.53.22\n")
-                .append("- timeout: 50\n")
-                .append("+ timeout: 20\n")
-                .append("+ verbose: true\n");
+        String rightAnswer = "{\n" +
+                "- follow: false\n" +
+                "  host: hexlet.io\n" +
+                "- proxy: 123.234.53.22\n" +
+                "- timeout: 50\n" +
+                "+ timeout: 20\n" +
+                "+ verbose: true\n" +
+                "}";
         var result = Differ.generate(firstJson, secondJson);
-        assertEquals(result, rightAnser);
+        assertEquals(result, rightAnswer);
     }
 
 }
