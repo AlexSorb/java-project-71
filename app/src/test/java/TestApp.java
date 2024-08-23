0877d65eef0e1b511c5f3979a6c70960fbbe8fb6
Alexandr Ryabinin <zochsa@gmail.com>
2024-08-23 13:28:39 +0300

    Bugfix

--- app/src/test/java/TestApp.java
+++ app/src/test/java/TestApp.java
@@ -31,14 +31,14 @@ public class TestApp {
     @Test
     public void testParsing() {
         System.out.println("Test1");
-        String rightAnswer = "{\n" +
-                "- follow: false\n" +
-                "  host: hexlet.io\n" +
-                "- proxy: 123.234.53.22\n" +
-                "- timeout: 50\n" +
-                "+ timeout: 20\n" +
-                "+ verbose: true\n" +
-                "}";
+        String rightAnswer = "{\n"
+                + "- follow: false\n"
+                + "  host: hexlet.io\n"
+                + "- proxy: 123.234.53.22\n"
+                + "- timeout: 50\n"
+                + "+ timeout: 20\n"
+                + "+ verbose: true\n"
+                + "}";
         var result = Differ.generate(firstJson, secondJson);
         assertEquals(result, rightAnswer);
     }
