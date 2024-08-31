a0ed03360618b79e5c57b0fe21c76e73c4ef3507
Alexandr Ryabinin <zochsa@gmail.com>
2024-08-31 16:35:07 +0300

    Bugfix

--- app/src/main/java/hexlet/code/source/Differ.java
+++ app/src/main/java/hexlet/code/source/Differ.java
@@ -9,8 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.HashMap;
 import java.util.ArrayList;
 //import java.util.*;
