72ab7b69fb0c4452ee694f101ce74e5a3f7a8492
Alexandr Ryabinin <zochsa@gmail.com>
2024-09-01 20:30:47 +0300

    add test

--- app/src/main/java/hexlet/code/source/FileManager.java
+++ app/src/main/java/hexlet/code/source/FileManager.java
@@ -1,24 +1,15 @@
 package hexlet.code.source;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 // Класс описывает работу с дерикотриями и прочтением данных из файла
 public class FileManager {
 
     public static Path normaolizePath(Path noNormPath) {
-        return noNormPath.normalize().toAbsolutePath();
-    }
 
-    public static String readData(Path file) {
-        List<String> dataFile;
-        try {
-            dataFile = Files.readAllLines(FileManager.normaolizePath(file));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return dataFile.toString();
+        var absolutPath = noNormPath.toAbsolutePath();
+        var normalizedPath = absolutPath.normalize();
+
+        return normalizedPath;
     }
 }
--- app/src/test/java/source/TestFIleManager.java
+++ app/src/test/java/source/TestFIleManager.java
@@ -1,4 +1,19 @@
 package source;
 
+import hexlet.code.source.FileManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Paths;
+
 public class TestFIleManager {
+
+    @Test
+    public void testNormalizePath() {
+        var notNormalizePath = Paths.get("./src/test/java/resources");
+        var normalizeAbsolutePath = Paths.get("src/test/java/resources").toAbsolutePath();
+        assertEquals(FileManager.normaolizePath(notNormalizePath), normalizeAbsolutePath);
+    }
+
+
 }
