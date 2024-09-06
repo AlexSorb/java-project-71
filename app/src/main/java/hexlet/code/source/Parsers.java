d782fa84ba122973bf35c10360531edb9b055eb5
Alexandr Ryabinin <zochsa@gmail.com>
2024-09-06 21:28:38 +0300

    transfer parsers into a Parsers class

--- app/src/main/java/hexlet/code/source/Parsers.java
+++ app/src/main/java/hexlet/code/source/Parsers.java
@@ -1,4 +1,23 @@
 package hexlet.code.source;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parsers {
+
+    public static Map<String, Object> parserJson(Path path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        var jsonMap = mapper.readValue(path.toFile(), Map.class);
+        return jsonMap;
+    }
+
+    public static Map<String, Object> parserYaml(Path file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        var yamlMap = mapper.readValue(file.toFile(), Map.class);
+        return yamlMap;
+    }
 }
